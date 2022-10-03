16fa7fde9ece00f62e16016a1b13f7219cfc4109
Monmcgt <71365435+Monmcgt@users.noreply.github.com>
2022-10-03 17:26:10 +0700

    Add "Initialise" Message

--- src/main/java/me/monmcgt/code/protostone/ProtoStone.java
+++ src/main/java/me/monmcgt/code/protostone/ProtoStone.java
@@ -3,15 +3,11 @@ package me.monmcgt.code.protostone;
 import me.monmcgt.code.protostone.util.PlayerManagerUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
 import net.minecraft.world.World;
 
 public class ProtoStone implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommonLifecycleEvents.TAGS_LOADED.register((__, ___) -> {
-        });
-
         ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
             // check whether the player has spawn point set or not
             boolean hasSpawnPoint = newPlayer.getSpawnPointPosition() != null;
@@ -26,5 +22,7 @@ public class ProtoStone implements ModInitializer {
 //                System.out.println("Teleported player to x: " + x + " y: " + y + " z: " + z);
             }
         });
+
+        Var.LOGGER.info("[ProtoStone] The mod has been initialised.");
     }
 }
